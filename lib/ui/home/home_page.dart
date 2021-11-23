ad38f29858574b766a19403c5eee5db587db22a0
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-23 14:33:00 +0800

    Buat BottomNavigationBar
    Didalam HomePage

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -32,10 +32,49 @@ class HomePage extends StatelessWidget {
             },
           ),
         ],
+        // bottom: PreferredSize(
+        //   preferredSize: Size.fromHeight(100.0),
+        //   child: Column(
+        //     children: [
+        //       Text('Coba'),
+        //       Text('Coba'),
+        //       Text('Coba'),
+        //       Text('Coba'),
+        //     ],
+        //   ),
+        // ),
       ),
-      body: Center(
-        child: Text('Home Page'),
+      body: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (BuildContext context, int i) {
+          return Container();
+        },
       ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: [
+            IconButton(icon: Icon(Icons.chevron_left), onPressed: () {}),
+            TextButton(
+              child: Text('Nov 2021'),
+              onPressed: () async {
+                final DateTime? newDate = await showDatePicker(
+                  context: context,
+                  initialDate: DateTime(2020, 11, 17),
+                  firstDate: DateTime(2017, 1),
+                  lastDate: DateTime(2022, 7),
+                  helpText: 'Select a date',
+                );
+              },
+            ),
+            IconButton(icon: Icon(Icons.chevron_right), onPressed: () {}),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
     );
   }
 }
