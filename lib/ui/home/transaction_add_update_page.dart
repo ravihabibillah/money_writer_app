1796833aa387dcd9925c540e12f4e4f78de3eb91
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-24 09:35:53 +0800

    Add DropdownButtonFormField
    Menambahkan DropdownButtonFormField dan mengubah TransactionAddUpdatePage menjadi StatefulWidget
    
    https://stackoverflow.com/questions/49780858/flutter-dropdown-text-field
    
    https://stackoverflow.com/questions/55600319/flutter-dropdownbuttonformfield-value-not-updating
    
    ✔ https://www.codegrepper.com/code-examples/dart/DropdownButtonFormField+flutter+example

--- lib/ui/home/transaction_add_update_page.dart
+++ lib/ui/home/transaction_add_update_page.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
-class TransactionAddUpdatePage extends StatelessWidget {
+class TransactionAddUpdatePage extends StatefulWidget {
   static const routeName = '/transaction_add_update_page';
 
   TransactionAddUpdatePage({Key? key}) : super(key: key);
 
+  @override
+  State<TransactionAddUpdatePage> createState() =>
+      _TransactionAddUpdatePageState();
+}
+
+class _TransactionAddUpdatePageState extends State<TransactionAddUpdatePage> {
   final _transactionFormKey = GlobalKey<FormState>();
+  String dropdownValue = '';
 
   @override
   Widget build(BuildContext context) {
@@ -32,8 +39,25 @@ class TransactionAddUpdatePage extends StatelessWidget {
               SizedBox(height: 16.0),
 
               // kategori
-              FormField(
-                builder: (FormFieldState<dynamic> field) {},
+              DropdownButtonFormField<String>(
+                items: <String>[
+                  'One',
+                  'Two',
+                  'Free',
+                  'Four',
+                ].map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    alignment: AlignmentDirectional.center,
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                value: dropdownValue,
+                onChanged: (String? newValue) {
+                  setState(() {
+                    dropdownValue = newValue!;
+                  });
+                },
               ),
               SizedBox(height: 16.0),
 
@@ -70,3 +94,50 @@ class TransactionAddUpdatePage extends StatelessWidget {
     );
   }
 }
+
+// class AppDropdownInput<T> extends StatelessWidget {
+//   final String hintText;
+//   final List<T> options;
+//   final T value;
+//   final String Function(T) getLabel;
+//   final Function(T) onChanged;
+//
+//   AppDropdownInput({
+//     this.hintText = 'Please select an Option',
+//     this.options = const [],
+//     required this.getLabel,
+//     required this.value,
+//     required this.onChanged,
+//   });
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return FormField<T>(
+//       builder: (FormFieldState<T> state) {
+//         return InputDecorator(
+//           decoration: InputDecoration(
+//             contentPadding:
+//                 EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
+//             labelText: hintText,
+//             border:
+//                 OutlineInputBorder(borderRadius: BorderRadius.circular(5.0)),
+//           ),
+//           isEmpty: value == null || value == '',
+//           child: DropdownButtonHideUnderline(
+//             child: DropdownButton<T>(
+//               value: value,
+//               isDense: true,
+//               onChanged: onChanged,
+//               items: options.map((T value) {
+//                 return DropdownMenuItem<T>(
+//                   value: value,
+//                   child: Text(getLabel(value)),
+//                 );
+//               }).toList(),
+//             ),
+//           ),
+//         );
+//       },
+//     );
+//   }
+// }
