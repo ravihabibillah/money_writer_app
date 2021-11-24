6995f5083b090bd0793817426304fac43a4423c2
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-24 09:10:31 +0800

    Form awal didalam TransactionAddUpdatePage

--- lib/ui/home/transaction_add_update_page.dart
+++ lib/ui/home/transaction_add_update_page.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 class TransactionAddUpdatePage extends StatelessWidget {
   static const routeName = '/transaction_add_update_page';
 
-  const TransactionAddUpdatePage({Key? key}) : super(key: key);
+  TransactionAddUpdatePage({Key? key}) : super(key: key);
+
+  final _transactionFormKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +13,59 @@ class TransactionAddUpdatePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Tambah Transaksi'),
       ),
-      body: Center(
-        child: Text('Transaksi Page'),
+      body: Form(
+        key: _transactionFormKey,
+        child: Container(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              // Tanggal
+              TextFormField(
+                decoration: InputDecoration(
+                  label: Text('tanggal'),
+                  icon: Icon(Icons.money),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16.0),
+
+              // kategori
+              FormField(
+                builder: (FormFieldState<dynamic> field) {},
+              ),
+              SizedBox(height: 16.0),
+
+              // Jumlah Uang
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  prefixText: 'Rp. ',
+                  label: Text('Jumlah'),
+                  icon: Icon(Icons.money),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16.0),
+
+              // Keterangan
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  label: Text('Keterangan'),
+                  icon: Icon(Icons.money),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16.0),
+            ],
+          ),
+        ),
       ),
     );
   }
