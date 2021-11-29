ada69ed0b55c6bac2161905f1cb52e743fce9eaf
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-29 11:56:33 +0800

    Template Awal ChartPage

--- lib/ui/chart/chart_page.dart
+++ lib/ui/chart/chart_page.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:month_picker_dialog/month_picker_dialog.dart';
 
-class ChartPage extends StatelessWidget {
+class ChartPage extends StatefulWidget {
   static const routeName = '/chart_page';
 
   const ChartPage({Key? key}) : super(key: key);
+
+  @override
+  State<ChartPage> createState() => _ChartPageState();
+}
+
+class _ChartPageState extends State<ChartPage> {
+  DateTime? selectedDate;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedDate = DateTime.now();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +25,78 @@ class ChartPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Grafik'),
       ),
-      body: Center(
-        child: Text('Chart Page'),
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Material(
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.chevron_left),
+                    onPressed: () {
+                      setState(() {
+                        selectedDate = DateTime(
+                          selectedDate!.year,
+                          selectedDate!.month - 1,
+                        );
+                      });
+                    },
+                  ),
+                  TextButton(
+                    child: Text(
+                      'Month: ${selectedDate?.month} - ${selectedDate?.year}',
+                    ),
+                    onPressed: () {
+                      showMonthPicker(
+                        context: context,
+                        firstDate: DateTime(DateTime.now().year - 10, 5),
+                        lastDate: DateTime(DateTime.now().year + 1, 9),
+                        initialDate: selectedDate ?? DateTime.now(),
+                        locale: const Locale("id"),
+                      ).then((date) {
+                        if (date != null) {
+                          setState(() {
+                            selectedDate = date;
+                          });
+                        }
+                      });
+                    },
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.chevron_right),
+                    onPressed: () {
+                      setState(() {
+                        selectedDate = DateTime(
+                          selectedDate!.year,
+                          selectedDate!.month + 1,
+                        );
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Divider(),
+          Column(
+            children: [
+              Text('Chart'),
+              SizedBox(height: 700),
+              const Divider(),
+              ListTile(
+                title: Text('title'),
+                trailing: Text('leading'),
+              ),
+              ListTile(
+                title: Text('title'),
+                trailing: Text('leading'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
