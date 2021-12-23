a0121b26b6924629a08f28d49f693cea9ef4cbcd
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-12-23 22:19:21 +0800

    Revert "update transaction_add_update_page, membuat tab view agar bisa bergeser"
    This reverts commit cf5c47eaa97a7e4aca2cd46ba9c09a67e6d2da02.

--- lib/ui/home/transaction_add_update_page.dart
+++ lib/ui/home/transaction_add_update_page.dart
@@ -103,68 +103,44 @@ class _TransactionAddUpdatePageState extends State<TransactionAddUpdatePage>
           ),
           body: Container(
             padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                // Mengubah kategori
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: TabBar(
-                    controller: _tabController,
-                    indicator: BoxDecoration(
-                        borderRadius:
-                            BorderRadius.circular(10), // Creates border
-                        color: _tabController.index == 0
-                            ? Colors.red
-                            : Colors.blue),
-                    labelColor: Colors.white,
-                    unselectedLabelColor: Colors.white,
-                    tabs: [
-                      Tab(
-                        text: "Pengeluaran",
-                      ),
-                      Tab(
-                        text: "Pemasukan",
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 16.0),
-                // Expanded(
-                //   child: TabBarView(
-                //     controller: _tabController,
-                //     children: <Widget>[
-                //       Column(
-                //         children: [
-                //           Text('coba'),
-                //         ],
-                //       ),
-                //       Column(
-                //         children: [
-                //           Text('coba'),
-                //         ],
-                //       ),
-                //     ],
-                //   ),
-                // ),
-                Form(
-                  key: _transactionFormKey,
-                  child: Expanded(
-                    child: TabBarView(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  // Mengubah kategori
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: TabBar(
                       controller: _tabController,
-                      children: <Widget>[
-                        _buildColumnTransaction(
-                            'pengeluaran', provider, context),
-                        _buildColumnTransaction('pemasukan', provider, context),
+                      indicator: BoxDecoration(
+                          borderRadius:
+                              BorderRadius.circular(10), // Creates border
+                          color: _tabController.index == 0
+                              ? Colors.red
+                              : Colors.blue),
+                      labelColor: Colors.white,
+                      unselectedLabelColor: Colors.white,
+                      tabs: [
+                        Tab(
+                          text: "Pengeluaran",
+                        ),
+                        Tab(
+                          text: "Pemasukan",
+                        ),
                       ],
                     ),
                   ),
-                  // child: _buildColumnTransaction(
-                  //     typeTransaction, provider, context),
-                ),
-              ],
+                  const SizedBox(height: 16.0),
+                  Form(
+                    key: _transactionFormKey,
+                    child: _buildColumnTransaction(
+                        typeTransaction, provider, context),
+                  ),
+                  // _buildFormTransaction(typeTransaction, provider, context),
+                ],
+              ),
             ),
           ),
         );
@@ -172,173 +148,171 @@ class _TransactionAddUpdatePageState extends State<TransactionAddUpdatePage>
     );
   }
 
-  Widget _buildColumnTransaction(String typeTransaction,
+  Column _buildColumnTransaction(String typeTransaction,
       TransactionsProvider provider, BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          // _buildTabTypeTransaction(),
-          // Tanggal
-          DateTimeField(
-            controller: _dateController,
-            initialValue: DateTime.tryParse(_dateController.text),
-            format: DateFormat("yyyy-MM-dd"),
-            decoration: InputDecoration(
-              label: const Text('tanggal'),
-              icon: const Icon(Icons.event),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(5.0),
-              ),
+    return Column(
+      children: [
+        // _buildTabTypeTransaction(),
+        // Tanggal
+        DateTimeField(
+          controller: _dateController,
+          initialValue: DateTime.tryParse(_dateController.text),
+          format: DateFormat("yyyy-MM-dd"),
+          decoration: InputDecoration(
+            label: const Text('tanggal'),
+            icon: const Icon(Icons.event),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5.0),
             ),
-            onShowPicker: (context, currentValue) {
-              return showDatePicker(
-                  context: context,
-                  firstDate: DateTime(1900),
-                  initialDate: currentValue ?? DateTime.now(),
-                  lastDate: DateTime(2100));
-            },
-            validator: (value) {
-              if (value == null) {
-                return 'Tanggal tidak boleh kosong';
-              }
-              return null;
-            },
           ),
-          const SizedBox(height: 16.0),
+          onShowPicker: (context, currentValue) {
+            return showDatePicker(
+                context: context,
+                firstDate: DateTime(1900),
+                initialDate: currentValue ?? DateTime.now(),
+                lastDate: DateTime(2100));
+          },
+          validator: (value) {
+            if (value == null) {
+              return 'Tanggal tidak boleh kosong';
+            }
+            return null;
+          },
+        ),
+        const SizedBox(height: 16.0),
 
-          // kategori
-          Consumer<CategoryProvider>(
-            builder: (context, provider, child) {
-              if (provider.statePengeluaran == ResultState.hasData ||
-                  provider.statePemasukan == ResultState.hasData) {
-                // data category
-                var getCategory = typePengeluaran
-                    ? provider.categoriesPengeluaran
-                    : provider.categoriesPemasukan;
+        // kategori
+        Consumer<CategoryProvider>(
+          builder: (context, provider, child) {
+            if (provider.statePengeluaran == ResultState.hasData ||
+                provider.statePemasukan == ResultState.hasData) {
+              // data category
+              var getCategory = typePengeluaran
+                  ? provider.categoriesPengeluaran
+                  : provider.categoriesPemasukan;
 
-                var categoryMap = getCategory.map((e) => {e.id: e.name});
+              var categoryMap = getCategory.map((e) => {e.id: e.name});
 
-                var categoryMapToDropdownMenuItem = categoryMap.map((entry) {
-                  return DropdownMenuItem(
-                    value:
-                        entry.keys.toString().replaceAll(RegExp(r'[^0-9]'), ''),
-                    child: Text(entry.values
-                        .toString()
-                        .replaceAll(RegExp(r'[^0-9\a-z\A-Z\ ]'), '')),
-                  );
-                });
+              var categoryMapToDropdownMenuItem = categoryMap.map((entry) {
+                return DropdownMenuItem(
+                  value:
+                      entry.keys.toString().replaceAll(RegExp(r'[^0-9]'), ''),
+                  child: Text(entry.values
+                      .toString()
+                      .replaceAll(RegExp(r'[^0-9\a-z\A-Z\ ]'), '')),
+                );
+              });
 
-                String? defaultValueDropdown() {
-                  if (widget.transaction?.idCategories.toString() != null) {
-                    if (widget.transaction?.type == typeTransaction) {
-                      return widget.transaction?.idCategories.toString();
-                    } else {
-                      return null;
-                    }
+              String? defaultValueDropdown() {
+                if (widget.transaction?.idCategories.toString() != null) {
+                  if (widget.transaction?.type == typeTransaction) {
+                    return widget.transaction?.idCategories.toString();
                   } else {
-                    dropdownValue = categoryMapToDropdownMenuItem.first.value;
-                    return categoryMapToDropdownMenuItem.first.value;
-                  }
-                }
-
-                return DropdownButtonFormField(
-                  items: categoryMapToDropdownMenuItem.toList(),
-                  value: defaultValueDropdown(),
-                  onChanged: (newValue) {
-                    dropdownValue = newValue as String?;
-                  },
-                  decoration: InputDecoration(
-                    label: const Text('Kategori'),
-                    icon: const Icon(Icons.category),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5.0),
-                    ),
-                  ),
-                  validator: (value) {
-                    if (value == null) {
-                      return 'Kategori tidak boleh kosong';
-                    }
                     return null;
-                  },
-                );
-              } else {
-                return Center(
-                  child: Text(provider.message),
-                );
-              }
-            },
-          ),
-          const SizedBox(height: 16.0),
-
-          // Jumlah Uang
-          TextFormField(
-            controller: _amountTextController,
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              prefixText: 'Rp. ',
-              label: const Text('Jumlah'),
-              icon: const Icon(Icons.money),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(5.0),
-              ),
-            ),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Masukkan jumlah Transaksi';
-              }
-              return null;
-            },
-          ),
-          const SizedBox(height: 16.0),
-
-          // Keterangan
-          TextFormField(
-            controller: _descriptionController,
-            decoration: InputDecoration(
-              label: const Text('Keterangan'),
-              icon: const Icon(Icons.description),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(5.0),
-              ),
-            ),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Keterangan Tidak boleh kosong';
-              }
-              return null;
-            },
-          ),
-          const SizedBox(height: 16.0),
-
-          ElevatedButton(
-            child: const Text('Simpan'),
-            onPressed: () {
-              if (_transactionFormKey.currentState!.validate()) {
-                var idTransaction = _isUpdate ? widget.transaction!.id : null;
-                var amountReplaceThousandSeparator = _amountTextController.text
-                    .replaceAll(RegExp(r'[^0-9]'), '');
-                int? amountToInt = int.tryParse(amountReplaceThousandSeparator);
-
-                Transactions dataTransaction = Transactions(
-                    id: idTransaction,
-                    description: _descriptionController.text,
-                    amount: amountToInt!,
-                    transactionDate: _dateController.text,
-                    idCategories: int.parse(dropdownValue!),
-                    type: typeTransaction);
-
-                if (!_isUpdate) {
-                  provider.addTransaction(dataTransaction);
+                  }
                 } else {
-                  provider.updateTransaction(dataTransaction);
+                  dropdownValue = categoryMapToDropdownMenuItem.first.value;
+                  return categoryMapToDropdownMenuItem.first.value;
                 }
-
-                Navigator.pop(context);
               }
-            },
+
+              return DropdownButtonFormField(
+                items: categoryMapToDropdownMenuItem.toList(),
+                value: defaultValueDropdown(),
+                onChanged: (newValue) {
+                  dropdownValue = newValue as String?;
+                },
+                decoration: InputDecoration(
+                  label: const Text('Kategori'),
+                  icon: const Icon(Icons.category),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null) {
+                    return 'Kategori tidak boleh kosong';
+                  }
+                  return null;
+                },
+              );
+            } else {
+              return Center(
+                child: Text(provider.message),
+              );
+            }
+          },
+        ),
+        const SizedBox(height: 16.0),
+
+        // Jumlah Uang
+        TextFormField(
+          controller: _amountTextController,
+          keyboardType: TextInputType.number,
+          decoration: InputDecoration(
+            prefixText: 'Rp. ',
+            label: const Text('Jumlah'),
+            icon: const Icon(Icons.money),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5.0),
+            ),
           ),
-        ],
-      ),
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Masukkan jumlah Transaksi';
+            }
+            return null;
+          },
+        ),
+        const SizedBox(height: 16.0),
+
+        // Keterangan
+        TextFormField(
+          controller: _descriptionController,
+          decoration: InputDecoration(
+            label: const Text('Keterangan'),
+            icon: const Icon(Icons.description),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+          ),
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Keterangan Tidak boleh kosong';
+            }
+            return null;
+          },
+        ),
+        const SizedBox(height: 16.0),
+
+        ElevatedButton(
+          child: const Text('Simpan'),
+          onPressed: () {
+            if (_transactionFormKey.currentState!.validate()) {
+              var idTransaction = _isUpdate ? widget.transaction!.id : null;
+              var amountReplaceThousandSeparator =
+                  _amountTextController.text.replaceAll(RegExp(r'[^0-9]'), '');
+              int? amountToInt = int.tryParse(amountReplaceThousandSeparator);
+
+              Transactions dataTransaction = Transactions(
+                  id: idTransaction,
+                  description: _descriptionController.text,
+                  amount: amountToInt!,
+                  transactionDate: _dateController.text,
+                  idCategories: int.parse(dropdownValue!),
+                  type: typeTransaction);
+
+              if (!_isUpdate) {
+                provider.addTransaction(dataTransaction);
+              } else {
+                provider.updateTransaction(dataTransaction);
+              }
+
+              Navigator.pop(context);
+            }
+          },
+        ),
+      ],
     );
   }
 
